8b3d710ac7a91c56bba903bd7bdb2380d216e800
Fran <fmvilas@gmail.com>
2015-09-17 17:55:55 +0200

    Fixing globals hash

--- lib/i18n/globals.rb
+++ lib/i18n/globals.rb
@@ -14,7 +14,7 @@ module I18n
   class << self
     def translate(*args)
       if args.last.is_a?(Hash)
-        args[-1] = config.globals.merge(args.last)
+        args[-1] = config.original_config.globals.merge(args.last)
       else
         args << config.globals
       end
